fd9e001d7129481e67e8513083eadf474eced0a6
Rupesh Kumar <Rupesh.kumar1@gs.com>
2019-12-01 23:53:06 +0530

    Arrays questions

--- src/arrays/ArraysExample.java
+++ src/arrays/ArraysExample.java
@@ -3,12 +3,12 @@ package arrays;
 public class ArraysExample {
     public static void main(String[] args) {
         ArraysExample arraysExample = new ArraysExample();
-        arraysExample.moveAllZerosToEnd();
+        arraysExample.removeDuplicates();
     }
 
     //Given an array of integers we need to move all the zeroes to the end and maintain the order of rest of the elements
     public void moveAllZerosToEnd() {
-        int[] numbers = {0,1, 2, 0, 5, 0, 7, 0, 9};
+        int[] numbers = {0, 1, 2, 0, 5, 0, 7, 0, 9};
         int length = numbers.length;
         int[] newArray = new int[length];
 
@@ -17,7 +17,7 @@ public class ArraysExample {
                 for (int j = i; j < length; j++) {
                     if (j == (length - 1)) {
                         numbers[j] = 0;
-                    }else{
+                    } else {
                         numbers[j] = numbers[j + 1];
                     }
 
@@ -28,5 +28,44 @@ public class ArraysExample {
         for (int i = 0; i < numbers.length; i++) {
             System.out.println(numbers[i]);
         }
+    }
+
+    //Remove duplicates from a sorted array
+
+    public void removeDuplicates() {
+        int[] numbers = {1, 2, 2, 3, 4, 5, 5, 6};
+        int count = 0;
+        int first = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (first == numbers[i]) {
+                count = count + 1;
+            }
+            first = numbers[i];
+        }
+        int lenthOfNewArray = numbers.length - count;
+
+        int[] newNumber = new int[lenthOfNewArray];
+        int ctn = 1;
+        first = numbers[0];
+        newNumber[0] = first;
+        for (int i = 1; i < numbers.length; i++) {
+            if (first == numbers[i]) {
+                continue;
+            } else {
+                newNumber[ctn] = numbers[i];
+                ctn = ctn + 1;
+                first = numbers[i];
+            }
+        }
+        for (int i = 0; i < newNumber.length; i++) {
+            System.out.println(newNumber[i]);
+        }
+    }
+
+    // Merge two sorted arrays
+
+    public void mergeSortedArray() {
+        int[] firstArray = {1, 2, 3, 4, 5};
+        int[] secondArray = {3, 4, 5, 6, 7};
     }
 }
