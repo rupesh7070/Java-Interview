495d07a4352d7e581feb0538e6906382659bfa9b
Rupesh Kumar <Rupesh.kumar1@gs.com>
2019-12-12 09:54:41 +0530

    Basic data structures

--- src/hashtable/BasicHashTable.java
+++ src/hashtable/BasicHashTable.java
@@ -1,4 +1,41 @@
 package hashtable;
 
-public class BasicHashTable {
+public class BasicHashTable<K, V> {
+    Entry[] data;
+    int capacity;
+
+    public BasicHashTable(int capacity) {
+        this.capacity = capacity;
+        data = new Entry[capacity];
+    }
+
+    public void put(K k, V v) {
+        Entry entry = new Entry(k, v);
+    }
+
+    private class Entry<K, V> {
+        private K key;
+        private V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public void setKey(K key) {
+            this.key = key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+    }
 }
